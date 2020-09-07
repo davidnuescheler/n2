c1cf7669d2fc8d8779f602f748e3ca86d0984038
David Nuescheler <david@nuescheler.ch>
2020-09-07 11:57:16 -0700

    Square Catalog Sync on 9/7/2020

--- filtered-catalog.js
+++ filtered-catalog.js
@@ -1488,16 +1488,6 @@ var catalog_raw=[
      }
     },
     {
-     "id": "DFFLG64I2Z7KWSTRN47IUGMM",
-     "item_variation_data": {
-      "item_id": "6IKBVSZV2JOTUFVZNNQSANM4",
-      "name": "COLUMBIA nora pillimue",
-      "price_money": {
-       "amount": 1600
-      }
-     }
-    },
-    {
      "id": "6GMLXEBTZ7A3Q4LB76YKOBRM",
      "item_variation_data": {
       "item_id": "6IKBVSZV2JOTUFVZNNQSANM4",
