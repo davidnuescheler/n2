21cd54debb0f0bce52ffc70ed7521d118caba988
David Nuescheler <david@nuescheler.ch>
2020-11-06 14:22:14 -0800

    Square Catalog Sync on 11/6/2020

--- filtered-catalog.js
+++ filtered-catalog.js
@@ -1560,16 +1560,6 @@ var catalog_raw=[
        "amount": 1600
       }
      }
-    },
-    {
-     "id": "7BYDCDBDYKMSUJSQHPVUEMIM",
-     "item_variation_data": {
-      "item_id": "6IKBVSZV2JOTUFVZNNQSANM4",
-      "name": "ETHIOPIA meaza",
-      "price_money": {
-       "amount": 1600
-      }
-     }
     }
    ]
   }
