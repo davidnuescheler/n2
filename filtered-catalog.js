95cffe2fee611da6015d348b22d61180792f642a
David Nuescheler <david@nuescheler.ch>
2020-12-19 12:52:20 -0800

    Square Catalog Sync on 12/19/2020

--- filtered-catalog.js
+++ filtered-catalog.js
@@ -5704,22 +5704,22 @@ var catalog_raw=[
    ],
    "variations": [
     {
-     "id": "5BHWBTFLADTKM6SKBGMBTMXP",
-     "item_variation_data": {
-      "item_id": "OZACLK5YWDZJW6MWDQHTCTNC",
-      "name": "half size",
-      "price_money": {
-       "amount": 500
-      }
-     }
-    },
-    {
      "id": "W7HA6AUW2UESIMMRBENFKBSC",
      "item_variation_data": {
       "item_id": "OZACLK5YWDZJW6MWDQHTCTNC",
       "name": "normal size",
       "price_money": {
        "amount": 700
+      }
+     }
+    },
+    {
+     "id": "5BHWBTFLADTKM6SKBGMBTMXP",
+     "item_variation_data": {
+      "item_id": "OZACLK5YWDZJW6MWDQHTCTNC",
+      "name": "half size",
+      "price_money": {
+       "amount": 500
       }
      }
     }
