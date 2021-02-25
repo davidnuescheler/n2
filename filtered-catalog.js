52934dbcc0df502b0f638a5876537bc8d12c9fa4
David Nuescheler <david@nuescheler.ch>
2021-02-25 11:17:18 -0800

    Square Catalog Sync on 2/25/2021

--- filtered-catalog.js
+++ filtered-catalog.js
@@ -9324,5 +9324,25 @@ var catalog_raw=[
     "amount": 0
    }
   }
+ },
+ {
+  "id": "773FGXNLDBJH5KYKIMKTJYTQ",
+  "type": "ITEM",
+  "item_data": {
+   "name": "amano pint",
+   "category_id": "IKMPD6MPVPOGD4MS4G4J3TQN",
+   "variations": [
+    {
+     "id": "LLMXKCVFJFOSJRS7RCCOK4ZY",
+     "item_variation_data": {
+      "item_id": "773FGXNLDBJH5KYKIMKTJYTQ",
+      "name": "Regular",
+      "price_money": {
+       "amount": 1200
+      }
+     }
+    }
+   ]
+  }
  }
 ]
