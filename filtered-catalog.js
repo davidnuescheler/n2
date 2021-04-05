e9d852151ee42b3962ec8a11abefdf63538af66d
David Nuescheler <david@nuescheler.ch>
2021-04-04 19:27:14 -0700

    Square Catalog Sync on 4/4/2021

--- filtered-catalog.js
+++ filtered-catalog.js
@@ -31,15 +31,6 @@ var catalog_raw=[
      }
     },
     {
-     "id": "KPYYYZJ4ZHLPRQ5CZ2P6ZZP2",
-     "modifier_data": {
-      "name": "chocolate sprinks (v) (gf)",
-      "price_money": {
-       "amount": 75
-      }
-     }
-    },
-    {
      "id": "2QIV3NVYJ3GMNRWDDZ7TMW6E",
      "modifier_data": {
       "name": "fruity pebbles (v) (gf)",
@@ -8642,15 +8633,6 @@ var catalog_raw=[
      }
     }
    ]
-  }
- },
- {
-  "id": "KPYYYZJ4ZHLPRQ5CZ2P6ZZP2",
-  "modifier_data": {
-   "name": "chocolate sprinks (v) (gf)",
-   "price_money": {
-    "amount": 75
-   }
   }
  },
  {
