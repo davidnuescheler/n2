d4227c69ad660f0eaa2f6d76cc36184a6973bd54
David Nuescheler <david@nuescheler.ch>
2021-04-27 21:32:15 -0600

    Square Catalog Sync on 4/27/2021

--- filtered-catalog.js
+++ filtered-catalog.js
@@ -1911,27 +1911,9 @@ var catalog_raw=[
      }
     },
     {
-     "id": "L5GACCA7C4RZELFDGBTOSMGH",
-     "modifier_data": {
-      "name": "salted vanilla bean (gf)",
-      "price_money": {
-       "amount": 0
-      }
-     }
-    },
-    {
      "id": "T7Y7ATH2M4VUVOLAS6ORUV5L",
      "modifier_data": {
       "name": "many-berry sorbet (v) (gf)",
-      "price_money": {
-       "amount": 0
-      }
-     }
-    },
-    {
-     "id": "UMPQB7IFMRRIQUPLUCFHND4X",
-     "modifier_data": {
-      "name": "salted vanilla bean + berry sorbet TWIST (gf)",
       "price_money": {
        "amount": 0
       }
@@ -2415,27 +2397,9 @@ var catalog_raw=[
      }
     },
     {
-     "id": "3TOCMG375GXASNKLGSYPX5SQ",
-     "modifier_data": {
-      "name": "salted vanilla bean (gf)",
-      "price_money": {
-       "amount": 300
-      }
-     }
-    },
-    {
      "id": "K7PU37Z3FQJKAVJZRU3DGKQ7",
      "modifier_data": {
       "name": "many berry sorbet (v) (gf)",
-      "price_money": {
-       "amount": 300
-      }
-     }
-    },
-    {
-     "id": "Q4L4EI7S4JUYWUHJWDKKBSRD",
-     "modifier_data": {
-      "name": "salted vanilla bean + berry sorbet TWIST (gf)",
       "price_money": {
        "amount": 300
       }
@@ -3710,30 +3674,10 @@ var catalog_raw=[
    ],
    "variations": [
     {
-     "id": "URNJNDMEVITDCFDRJT5X7LA5",
-     "item_variation_data": {
-      "item_id": "CVMOVN75J6NADSS7GEG4WMFW",
-      "name": "salted vanilla bean (gf)",
-      "price_money": {
-       "amount": 0
-      }
-     }
-    },
-    {
      "id": "D5YA2V4OC7V4XGCSCP5BR53A",
      "item_variation_data": {
       "item_id": "CVMOVN75J6NADSS7GEG4WMFW",
       "name": "many-berry sorbet (v) (gf)",
-      "price_money": {
-       "amount": 0
-      }
-     }
-    },
-    {
-     "id": "XVSSPITWBOP6YVYCWWJJUNWV",
-     "item_variation_data": {
-      "item_id": "CVMOVN75J6NADSS7GEG4WMFW",
-      "name": "salted vanilla bean + berry sorbet TWIST (gf)",
       "price_money": {
        "amount": 0
       }
@@ -9559,42 +9503,6 @@ var catalog_raw=[
   "id": "BQ75KOYH53HQ3B4LHPOHYV7Q",
   "modifier_data": {
    "name": "guava + olive oil TWIST (gf)",
-   "price_money": {
-    "amount": 300
-   }
-  }
- },
- {
-  "id": "L5GACCA7C4RZELFDGBTOSMGH",
-  "modifier_data": {
-   "name": "salted vanilla bean (gf)",
-   "price_money": {
-    "amount": 0
-   }
-  }
- },
- {
-  "id": "UMPQB7IFMRRIQUPLUCFHND4X",
-  "modifier_data": {
-   "name": "salted vanilla bean + berry sorbet TWIST (gf)",
-   "price_money": {
-    "amount": 0
-   }
-  }
- },
- {
-  "id": "3TOCMG375GXASNKLGSYPX5SQ",
-  "modifier_data": {
-   "name": "salted vanilla bean (gf)",
-   "price_money": {
-    "amount": 300
-   }
-  }
- },
- {
-  "id": "Q4L4EI7S4JUYWUHJWDKKBSRD",
-  "modifier_data": {
-   "name": "salted vanilla bean + berry sorbet TWIST (gf)",
    "price_money": {
     "amount": 300
    }
