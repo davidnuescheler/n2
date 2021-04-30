01700a1b2e75f26dcbe7bed46ebefb662403c8c5
David Nuescheler <david@nuescheler.ch>
2021-04-30 14:57:18 -0600

    Square Catalog Sync on 4/30/2021

--- filtered-catalog.js
+++ filtered-catalog.js
@@ -9609,5 +9609,25 @@ var catalog_raw=[
     "amount": 75
    }
   }
+ },
+ {
+  "id": "LZWYZ6SL545J65XP67IKY75V",
+  "type": "ITEM",
+  "item_data": {
+   "name": "cactus cooler pint (v)",
+   "category_id": "IKMPD6MPVPOGD4MS4G4J3TQN",
+   "variations": [
+    {
+     "id": "7RWWRWFJP4OZPMITAOXXGJQS",
+     "item_variation_data": {
+      "item_id": "LZWYZ6SL545J65XP67IKY75V",
+      "name": "Regular",
+      "price_money": {
+       "amount": 1200
+      }
+     }
+    }
+   ]
+  }
  }
 ]
